d58fc96a498fd7a8683e7a8c8ae4001ab4b9f128
Gina <ginavila30@icloud.com>
2022-04-09 12:35:09 -0400

    untracking .idea folder Adding more lines to AllPeople Class

--- src/day42_custom_classes/AllPeople.java
+++ src/day42_custom_classes/AllPeople.java
@@ -2,9 +2,26 @@ package day42_custom_classes;
 
 public class AllPeople {
     public static void main(String[] args) {
-        Person personOne=new Person(); // This is how we create an object->instance form the Person class.
-        Person personTwo=new Person();// Everytime I make an object I use keyword new.
+        Person personOne = new Person(); // This is how we create an object->instance form the Person class.
+        Person personTwo = new Person();// Everytime I make an object I use keyword new.
 
-        personOne.age=27;
+        //Accessing the instances variables
+        personOne.age = 27;
+        personOne.name = "Gina";
+        personOne.height = 1.50;
+        personOne.isMarried = true;
+
+        //Printing the instances variables for person 1 (I already assign info for this person)
+        System.out.println(personOne.age);
+        System.out.println(personOne.name);
+        System.out.println(personOne.height);
+        System.out.println(personOne.isMarried);
+
+        //Printing the instances variables for person 2 (I did not assign info for this person). I will see default values.
+        System.out.println(personTwo.age);
+        System.out.println(personTwo.name);
+        System.out.println(personTwo.height);
+        System.out.println(personTwo.isMarried);
+
     }
 }
