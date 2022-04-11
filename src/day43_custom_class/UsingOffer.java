05494f6c65d454d2511bf74c353e45ac50fde4b7
Gina <ginavila30@icloud.com>
2022-04-10 21:44:15 -0400

    Committing work done on 4/10/2022

---
+++ src/day43_custom_class/UsingOffer.java
@@ -0,0 +1,15 @@
+package day43_custom_class;
+
+public class UsingOffer {
+    public static void main(String[] args) {
+        Offer company1=new Offer("Oracle","VA");
+        System.out.println(company1);
+        System.out.println("=========================================================");
+        Offer company2=new Offer("VA","Oracle",150000,true,15);
+        System.out.println(company2);
+        System.out.println("=========================================================");
+        Offer company3=new Offer("Oracle","VA",150000);
+        System.out.println(company3);
+
+    }
+}
