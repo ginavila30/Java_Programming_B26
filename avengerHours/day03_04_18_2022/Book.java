8f71196df05c19cee30ef2e7466cc78cee1441ff
Gina <ginavila30@icloud.com>
2022-04-17 21:35:23 -0400

    New files added

---
+++ avengerHours/day03_04_18_2022/Book.java
@@ -0,0 +1,55 @@
+package day03_04_18_2022;
+
+public class Book {
+
+    //Instance Vx
+    String name;
+    String category;
+    String author;
+    String bookId;
+    int shelfNum;
+    //Static Vx
+    static int bookCount;
+    //static block
+    static {
+        bookCount=0;
+    }
+    //Constructor
+    public Book(String name){
+        this.name=name;   bookCount++;
+    }
+    public Book(String name, String category){
+        this(name);
+        this.category=category;
+    }
+    public Book (String name, String category, String author){
+     this(name,category);
+     this.author=author;
+
+    }
+
+
+ @Override
+ public String toString() {
+  return "Book->" +
+          (name == null ?"": "Name= " + name + ", " )+
+          (category == null ?"": "Category= " + category + ", " )+
+          (author == null ?"": "Author= " + author + ", " )+
+          ( bookId == null ?"": "Book ID= " + bookId + ", " )+
+          ( shelfNum == 0 ?"": "ShelfNum= "+shelfNum+", ");
+ }
+
+ public void IdGenerator(){
+        if(category!=null){
+  switch (category.toLowerCase()){
+   case "adventure":shelfNum=1;
+   case "classics":shelfNum=2;
+   case "comics":shelfNum=3;
+   case "fantasy":shelfNum=4;
+   case "history":shelfNum=5;
+   default:shelfNum=0;
+  }}
+if(author!=null){
+  bookId=shelfNum+"_"+author.charAt(0)+"_"+name.substring(0,2);}
+}
+}
---
+++ avengerHours/day03_04_18_2022/Library.java
@@ -0,0 +1,31 @@
+package day03_04_18_2022;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Library {
+    public static void main(String[] args) {
+        /*-Create a class named Library
+        -create Book objects
+        -find the number off all books in the
+        library with the help of "static" feature
+        -create allBooks arrayList
+        -create fantasyBooks arraylist by using allBooks collection
+*/
+        Book book1=new Book("ABC");
+        Book book2=new Book("The numbers","kids");
+        Book book3=new Book("Dragons","fantasy","James R");
+
+        System.out.println(Book.bookCount); // each constructor increases objects.
+
+        book1.IdGenerator();
+        book2.IdGenerator();
+        book3.IdGenerator();
+
+        ArrayList<Book>allBooks=new ArrayList<>(Arrays.asList(book1,book2,book3));
+        System.out.println(allBooks);
+        ArrayList<Book>fantasyBooks=new ArrayList<>(allBooks);
+        fantasyBooks.removeIf(each->each.category==null||!each.category.equalsIgnoreCase("fantasy"));
+        System.out.println(fantasyBooks);
+    }
+}
