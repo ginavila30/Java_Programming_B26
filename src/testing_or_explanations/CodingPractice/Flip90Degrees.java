95dad18f848d6c82e2fe2fa52a2b3ecd003482e9
Gina <ginavila30@icloud.com>
2022-04-26 12:43:45 -0400

    New files added

---
+++ src/testing_or_explanations/CodingPractice/Flip90Degrees.java
@@ -0,0 +1,38 @@
+package testing_or_explanations.CodingPractice;
+
+import java.util.Arrays;
+
+/*java flip the picture (90 degree to the right) i.a. flip the first raw to make it last column && last column to make last raw
+
+        /* int [][] cube = {
+
+                          {11, 12, 13, 14},
+                          {21, 22, 23, 24},
+                          {31, 32, 33, 34},
+                          {41, 42, 43, 44}
+
+                          };
+
+         */
+public class Flip90Degrees {
+    public static void main(String[] args) {
+        int [][] cube = {
+
+                {11, 12, 13, 14},//[41, 31, 21, 11]
+                {21, 22, 23, 24},//[42, 32, 22, 12]
+                {31, 32, 33, 34},//[43, 33, 23, 13]
+                {41, 42, 43, 44}// [44, 34, 24, 14]
+
+        };
+        int[][]cubeCopy= new int[cube.length][cube[0].length];
+
+        for (int i = 0, l= cube.length-1; i < cube.length ; i++,l--) {
+            for (int j = 0; j < cube.length ; j++) {
+              int each =cube[i][j];
+                cubeCopy[j][l]=each;
+            }
+        }
+        System.out.println(Arrays.deepToString(cubeCopy));
+
+    }
+}
