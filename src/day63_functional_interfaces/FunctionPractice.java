c364906fa192cabc6e59ba9815cb00f3aa8ec25e
Gina <ginavila30@icloud.com>
2022-05-30 01:14:47 -0400

    New files added

---
+++ src/day63_functional_interfaces/FunctionPractice.java
@@ -0,0 +1,56 @@
+package day63_functional_interfaces;
+
+import java.util.*;
+import java.util.function.Function;
+/*Use Function functional interface to:
+		1.1 Create a function that can return the sum of digits from a string that contains number digits
+        1.2 Create a function that can convert a Set of Integers to List of Integers
+		1.3 Create a function that can return the reversed version of a String array*/
+public class FunctionPractice {
+    public static void main(String[] args) {
+        Function<String,Integer>sumOfDigitsStr = str->{
+            Integer result =0;
+            String temp="";
+            for (int i = 0; i < str.length() ; i++) {
+                if(Character.isDigit(str.charAt(i))){
+                    temp+=str.charAt(i);
+                }
+                if(Character.isLetter(str.charAt(i))||i==str.length()-1){
+                    if(temp.length()>0){
+                        result+=Integer.parseInt(temp);
+                        temp="";
+                    }
+                }
+            }
+            return result;
+        };
+        System.out.println(sumOfDigitsStr.apply("2h4k3h39"));
+        //1.2 Create a function that can convert a Set of Integers to List of Integers
+        Function<Set<Integer>, List<Integer>> convertIntegerSetToList= set->{
+           List<Integer>list=new ArrayList<>(set);
+            return list;
+        };
+        HashSet<Integer>set=new HashSet<>();
+        set.add(5);
+        set.add(6);
+        set.add(7);
+        set.add(10);
+        System.out.println(convertIntegerSetToList.apply(new HashSet<>(set)));
+
+        //1.3 Create a function that can return the reversed version of a String array
+
+        Function<String[],String []> reverseStringArray= arr ->{
+           String[]reversed=new String[arr.length];
+            for (int i = 0; i < arr.length ; i++) {
+                String reversedStr="";
+                for (int j = arr[i].length()-1; j>=0 ; j--) {
+                    reversedStr+=arr[i].charAt(j);
+                }
+                reversed[i]=reversedStr;
+            }
+            return  reversed;
+        };
+
+        System.out.println(Arrays.toString(reverseStringArray.apply(new String[]{"ice", "cat", "hot", "dog"})));
+    }
+}
--- src/day63_functional_interfaces/PredicatePractice.java
+++ src/day63_functional_interfaces/PredicatePractice.java
@@ -7,13 +7,7 @@ import java.util.function.Predicate;
 
 /*Use Predicate Functional interface to:
 		1.1 Create a function that can verify if a number contains duplicate digits
-
-		1.2 Create a function that can verify if a password is a strong password. Characteristics of strong passwords are:
-		        Password must be at least have 8 characters long, and should not contain space
-		        Password should at least contain one upper case letter
-		        Password should at least contain one lower case letter
-		        Password should at least contain one special characters
-		        Password should at least contain a digit*/
+*/
 public class PredicatePractice {
     public static void main(String[] args) {
         Predicate<Integer>verifyDuplicateDigits= (num)->{
@@ -32,6 +26,40 @@ public class PredicatePractice {
             return valid;
         };
 
-        System.out.println(verifyDuplicateDigits.test(15751));
+        System.out.println(verifyDuplicateDigits.test(157));
+        //1.2 Create a function that can verify if a password is a strong password. Characteristics of strong passwords are:
+        //		        Password must be at least have 8 characters long, and should not contain space
+        //		        Password should at least contain one upper case letter
+        //		        Password should at least contain one lower case letter
+        //		        Password should at least contain one special characters
+        //		        Password should at least contain a digit
+
+        Predicate<String>verifyStrongPassword= password->{
+            boolean upperCase=false;
+            boolean lowerCase=false;
+            boolean specialChar=false;
+            boolean digit=false;
+
+
+            if(password.length()>=8&&!password.contains(" ")){
+                for (int i = 0; i < password.length() ; i++) {
+                    if(Character.isUpperCase(password.charAt(i))){
+                        upperCase=true;
+                    }
+                    if(Character.isLowerCase(password.charAt(i))){
+                        lowerCase=true;
+                    }
+                    if(!Character.isAlphabetic(password.charAt(i))){
+                        specialChar=true;
+                    }
+                    if(Character.isDigit(password.charAt(i))){
+                        digit=true;
+                    }
+                }
+                return upperCase&&lowerCase&&specialChar&&digit;
+            }
+            return false;
+        };
+        System.out.println(verifyStrongPassword.test("Ilovem@jid123!"));
     }
 }
