19fd88ad41e91019a197a3be61a0be48ab273ff3
Gina <ginavila30@icloud.com>
2022-06-19 20:05:46 -0400

    New files added

---
+++ src/testing_or_explanations/dardanTasks/MapsPractice.java
@@ -0,0 +1,55 @@
+package testing_or_explanations.dardanTasks;
+
+import java.util.*;
+
+/*Questions for next week
+
+Map -- Sort the map by values
+Write a method that can sort the Map by values
+
+
+
+Map -- Min value
+Write a method that can return the minimum value from ta map (DO NOT use sort method)*/
+public class MapsPractice {
+    public static void main(String[] args) {
+        System.out.println(frequencyOfChars("apple"));
+        System.out.println(uniqueChars("apple"));
+        Map<Integer,Integer>map=new HashMap<>();
+        map.put(2,4);
+        map.put(1,-3);
+        map.put(9,8);
+        map.put(3,2);
+        map.put(25,1);
+        System.out.println(minimumValue(map));
+    }
+    public static Map<String,Integer> frequencyOfChars(String str){
+        Map<String, Integer>map=new LinkedHashMap<>();
+        ArrayList<String>list=new ArrayList<>(Arrays.asList(str.split("")));
+        for (String each:list) {
+            map.put(each,Collections.frequency(list,each));
+        }
+        return map;
+    }
+    public static Map<String,Integer> uniqueChars(String str){
+        Map<String, Integer>map=new LinkedHashMap<>();
+        ArrayList<String>list=new ArrayList<>(Arrays.asList(str.split("")));
+        for (String each:list) {
+            if(Collections.frequency(list,each)==1)
+            map.put(each,Collections.frequency(list,each));
+        }
+        return map;
+    }
+    public static int minimumValue(Map<Integer,Integer>map) {
+        int min = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, Integer> each : map.entrySet()) {
+            if (each.getValue() < min) {
+                min = each.getValue();
+            }
+        }
+        return min;
+    }
+//    public static Map <Integer,Integer> sortByValue(Map<Integer,Integer>map){
+//
+//    }
+}
